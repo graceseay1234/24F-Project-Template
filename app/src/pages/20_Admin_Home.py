dde4a68a66465cc04309ab4d26f5bd2fa1a5b591
Anne Hu <Anne20052005@hotmail.com>
2024-12-02 13:35:56 -0500

    Complete SQL code for database
    Added the entire SQL database creation code.

--- app/src/pages/20_Admin_Home.py
+++ app/src/pages/20_Admin_Home.py
@@ -3,15 +3,35 @@ logger = logging.getLogger(__name__)
 
 import streamlit as st
 from modules.nav import SideBarLinks
-import requests
 
 st.set_page_config(layout = 'wide')
 
+# Show appropriate sidebar links for the role of the currently logged in user
 SideBarLinks()
 
-st.title('System Admin Home Page')
+st.title(f"Warehouse Manager Portal")
+st.write('')
+st.write('')
+st.write('### Reports')
 
-if st.button('Update ML Models', 
+if st.button('Show All Reorders',
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/21_ML_Model_Mgmt.py')
+  st.switch_page('pages/41_Reorders.py')
+
+if st.button('Show Low Stock',
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/42_Low_Stock.py')
+
+st.write('### New Products and Categories')
+
+if st.button('Add New Product Category',
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/43_New_Cat.py')
+
+if st.button('Add New Product',
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/44_New_Product.py')
