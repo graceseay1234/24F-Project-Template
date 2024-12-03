c6e93f5ab7ca58e3dd65d31780255db1522bc404
blake <47119267+blakewaldner@users.noreply.github.com>
2024-12-03 15:00:17 -0500

    added base layout for student home

--- app/src/pages/31_Student_Home.py
+++ app/src/pages/31_Student_Home.py
@@ -1,1 +1,25 @@
 #STUDENT
+import logging
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from modules.nav import SideBarLinks
+try:
+    import streamlit_antd_components as sac
+except ModuleNotFoundError:
+    import os
+    os.system('pip install streamlit-antd-components')
+    import streamlit_antd_components as sac
+
+logger = logging.getLogger(__name__)
+
+# Show appropriate sidebar links for the role of the currently logged in user
+SideBarLinks()
+
+# Header and personalized greeting
+st.markdown('<h1 style="font-size: 50px;font-weight: 300;">Student Dashboard</h1>', unsafe_allow_html=True) 
+
+st.markdown('<p class="light-text" style="font-size: 24px;">Welcome, Student.</p>', unsafe_allow_html=True)
+
+sac.divider(align='center', color='gray')
