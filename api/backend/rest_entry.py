917bdc214baf13f223aa6073d9fc60a30ca7178f
LolurMom <graceseay1234@gmail.com>
2024-12-05 21:28:24 -0500

    Update rest_entry.py

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -2,8 +2,6 @@ from flask import Flask
 
 from backend.db_connection import db
 from backend.customers.customer_routes import customers
-from backend.products.products_routes import products
-from backend.simple.simple_routes import simple_routes
 from backend.alumni.alumni_routes import alumni
 from backend.system_admin.systemadmin_routes import administrator
 from backend.feedback.feedback_routes import feedback
